a0454fa4ede3d328f4d3c27a37013b8e33b05b93
Ramachandrareddy <42475784+ramachandrareddyk@users.noreply.github.com>
2023-03-16 08:41:41 +0530

    added some new methods

--- src/main/java/Com/Testing/Abc.java
+++ src/main/java/Com/Testing/Abc.java
@@ -6,6 +6,14 @@ public class Abc {
 		// TODO Auto-generated method stub
 
 		System.out.println("This is my abc class");
+		
+		int x=add(25, 56);
+		
+		System.out.println(x);
+	}
+	
+	public static int add(int a, int b) {
+		return a+b;
 	}
 
 }
--- src/test/java/Com/Testing/Base.java
+++ src/test/java/Com/Testing/Base.java
@@ -6,6 +6,16 @@ public class Base {
 		// TODO Auto-generated method stub
 
 		System.out.println("This is my base class");
+		
+		compare("Testing", "Test");
+	}
+	
+	public static void compare(String a, String b) {
+		if(a.equalsIgnoreCase(b)) {
+			System.out.println("Both are equal");
+		} else {
+			System.out.println("Both are not equal");
+		}
 	}
 
 }
