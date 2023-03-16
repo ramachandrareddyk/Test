121b6ccd570d88c10ca312ace6f93e49b1700665
Ramachandrareddy <42475784+ramachandrareddyk@users.noreply.github.com>
2023-03-16 08:34:51 +0530

    Added some classes

---
+++ src/main/java/Com/Testing/Test1.java
@@ -0,0 +1,11 @@
+package Com.Testing;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("This is mt test 1 class");
+	}
+
+}
