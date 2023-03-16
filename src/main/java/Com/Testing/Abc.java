89eeba7150ceefa14d0fcc0771dc22e204cb7806
Ramachandrareddy <42475784+ramachandrareddyk@users.noreply.github.com>
2023-03-16 08:21:59 +0530

    new project

---
+++ src/main/java/Com/Testing/Abc.java
@@ -0,0 +1,11 @@
+package Com.Testing;
+
+public class Abc {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("This is my abc class");
+	}
+
+}
---
+++ src/test/java/Com/Testing/Tester.java
@@ -0,0 +1,10 @@
+package Com.Testing;
+
+public class Tester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("This is my test class");
+	}
+
+}
